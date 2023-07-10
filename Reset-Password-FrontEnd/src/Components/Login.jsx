5f1ee3eda4b12d50f2d346a54791d952949f6675
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-07-10 21:25:32 +0530

    Update Login.jsx

--- Reset-Password-FrontEnd/src/Components/Login.jsx
+++ Reset-Password-FrontEnd/src/Components/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
         try {
 
 
-            const response = await fetch(`http://localhost:3005/user/login`, {
+            const response = await fetch(`https://password-reset-task01.onrender.com/user/login`, {
                 method: "POST",
                 body: JSON.stringify(login),
                 headers: {
